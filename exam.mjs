f0853e84ba83d6234dce0ace984758d15adcc449
Yazid Habibi <jusuf1909@student.abudzar.sch.id>
2024-10-03 09:27:48 +0700

    p3

--- exam.mjs
+++ exam.mjs
@@ -2,11 +2,9 @@ export function analisisdata() {
   let result = [];
   for (let i = 1; i <= 20; i++) {
     let divisibleByFour = i % 4 === 0 ? "bisa dibagi 4" : "tidak bisa dibagi 4";
-    let oddEven = i % 2 === 0 ? `genap ${divisibleByFour}` : `ganjil`;
+    let oddEven = i % 2 === 0 ? `genap ${"dan " + divisibleByFour}` : `ganjil`;
 
     result.push(`${i} adalah bilangan ${oddEven}`);
   }
   return result;
 }
-
-console.log(analisisdata());
